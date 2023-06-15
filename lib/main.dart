b8ae64db4c2fe2820f81fc85194f8bcc137be8bf
prem0556 <kumarp73616@gmail.com>
2023-06-15 07:20:57 +0530

    app is utilised

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
       title: 'To-Do App',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: const TodoListScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
